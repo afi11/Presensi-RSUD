3c081d4d8c5e190a4487c0d309c2148d2851156d
afif.fatakhul <fatakhulafi11@gmail.com>
2022-12-18 10:51:09 +0700

    Update URL

--- src/config/Url.js
+++ src/config/Url.js
@@ -1,6 +1,6 @@
-// export const apiUrl = 'http://182.253.66.122:2022/presensi-web-rsud/public/api';
-// export const urlAssetImageProfil = 'http://182.253.66.122:2022/presensi-web-rsud/public/assets/img/users/';
-// export const urlAssetIzin = 'http://182.253.66.122:2022/presensi-web-rsud/public/api/fetch-file-izin/';
-export const apiUrl = 'http://192.168.254.203/api';
-export const urlAssetImageProfil = 'http://192.168.254.203/assets/img/users/';
-export const urlAssetIzin = 'http://192.168.254.203/api/fetch-file-izin/';
+export const apiUrl = 'http://182.253.66.122:2022/presensi-web-rsud/public/api';
+export const urlAssetImageProfil = 'http://182.253.66.122:2022/presensi-web-rsud/public/assets/img/users/';
+export const urlAssetIzin = 'http://182.253.66.122:2022/presensi-web-rsud/public/api/fetch-file-izin/';
+// export const apiUrl = 'http://192.168.254.203/api';
+// export const urlAssetImageProfil = 'http://192.168.254.203/assets/img/users/';
+// export const urlAssetIzin = 'http://192.168.254.203/api/fetch-file-izin/';
